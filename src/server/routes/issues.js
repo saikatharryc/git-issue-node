d6a005069180f6add76a32c8ff2b9fbede0fd27e
saikat <saikatchakrabortty2@gmail.com>
2019-04-10 13:21:39 +0530

    functions chained up

--- src/server/routes/issues.js
+++ src/server/routes/issues.js
@@ -2,6 +2,34 @@ const express = require("express");
 const router = express.Router();
 const issuesCont = require("../controllers/issues.cont");
 
-router.get("/issues", (req, res, next) => {});
+router.post("/locateRepo", (req, res, next) => {
+  const { userName, repoName } = req.body;
+  if (!userName || !repoName) {
+    return next({ status: 404, message: "please pass all the params" });
+  }
+  return issuesCont
+    .locateRepo(userName, repoName)
+    .then(data => {
+      return res.json(data);
+    })
+    .catch(error => {
+      return next(error);
+    });
+});
+
+router.get("/issues", (req, res, next) => {
+  const { userName, repoName, page = 1 } = req.body;
+  if (!userName || !repoName) {
+    return next({ status: 404, message: "please pass all the params" });
+  }
+  return issuesCont
+    .getIssues(userName, repoName, page)
+    .then(data => {
+      return res.json(data);
+    })
+    .catch(error => {
+      return next(error);
+    });
+});
 
 module.exports = router;
