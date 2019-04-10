276b06713586322823ef90ace31e2d08b0cb69d6
saikat <saikatchakrabortty2@gmail.com>
2019-04-10 12:26:49 +0530

    Helper functions added

--- src/server/config/index.js
+++ src/server/config/index.js
@@ -4,6 +4,9 @@ const config = {
       process.env.MONGO_URL ||
       "mongodb://<admin>:<password>@<host>:<port>/<db name | default admin>",
     OPTIONS: { useNewUrlParser: true }
+  },
+  github: {
+    base: "https://api.github.com"
   }
 };
 Object.freeze(config);
---
+++ src/server/helpers/issues.helper.js
@@ -0,0 +1,37 @@
+const rp = require("request-promise");
+const githubApiBase = require("../config").github.base;
+
+const locateRepo = async (username, reponame) => {
+  const options = {
+    uri: githubApiBase + "/repos/" + username + "/" + reponame,
+    json: true
+  };
+  try {
+    const repoData = await rp(options);
+    return repoData;
+  } catch (ex) {
+    return Promise.reject({ message: ex.message || "unknown error occured." });
+  }
+};
+
+const getIssuesByRepo = async (username, reponame, page = 1) => {
+  const options = {
+    uri: githubApiBase + "/repos/" + username + "/" + reponame + "/issues",
+    qs: {
+      per_page: 20,
+      page: page
+    },
+    json: true
+  };
+  try {
+    const issueData = await rp(options);
+    return issueData;
+  } catch (ex) {
+    return Promise.reject({ message: ex.message || "unknown error occured." });
+  }
+};
+
+module.exports = {
+  locateRepo,
+  getIssuesByRepo
+};
